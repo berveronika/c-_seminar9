ea7b714fc46fdc1e3191d6f9feed27df56831c67
«Veronika <berezovskaya.veronika1998@gmail.com>
2023-03-15 11:12:57 +0500

    задача 1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
 ﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
+/*
+void ShowNumbers(int number)
+{
+    Console.Write(number + " ");
+    if (number > 1) ShowNumbers(number - 1);
+    }
 
-
-
+Console.Write("Input a natural number: ");
+int user_n = Convert.ToInt32(Console.ReadLine());
+if (user_n > 0) ShowNumbers(user_n);
+else Console.WriteLine("Impossible value!");
+*/
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
