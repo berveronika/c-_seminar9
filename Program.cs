175c30266c3483ed9041a91dd3e63bd8a390e232
«Veronika <berezovskaya.veronika1998@gmail.com>
2023-03-15 12:01:06 +0500

    задача 3

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ else Console.WriteLine("Impossible value!");
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
-int SumNumbersMN(int m, int n, int sum)
+/* int SumNumbersMN(int m, int n, int sum)
 {
     sum += m;
     if (m < n) return SumNumbersMN(m + 1, n, sum);
@@ -33,11 +33,25 @@ if (user_m < user_n)
     Console.WriteLine($"The sum of natural numbers between {user_m} and {user_n} is {sumBetweenMN}!");
 }
 else Console.WriteLine("Impossible value!");
-
-
-
+*/
 
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
+/*
+int Ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return Ackermann(m - 1, 1);
+    else return Ackermann(m - 1, Ackermann(m, n - 1));
+}
 
+Console.Write("Input M for Ackermann function: ");
+int user_m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input N for Ackermann function: ");
+int user_n = Convert.ToInt32(Console.ReadLine());
+
+if (user_m >= 0 && user_n >= 0)
+    Console.WriteLine($"A({user_m},{user_n}) = {Ackermann(user_m, user_n)}");
+else Console.WriteLine("Impossible value!");
+*/
