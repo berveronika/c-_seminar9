5f2ae280fe965dcf22da00e7d925a84a66d3d166
«Veronika <berezovskaya.veronika1998@gmail.com>
2023-03-15 11:54:05 +0500

    задача 2

--- Program.cs
+++ Program.cs
@@ -15,6 +15,24 @@ else Console.WriteLine("Impossible value!");
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
+int SumNumbersMN(int m, int n, int sum)
+{
+    sum += m;
+    if (m < n) return SumNumbersMN(m + 1, n, sum);
+    else return sum;
+}
+
+Console.Write("Input a number: ");
+int user_m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input one more number: ");
+int user_n = Convert.ToInt32(Console.ReadLine());
+if (user_m < user_n)
+{
+    int sumBetweenMN = 0;
+    sumBetweenMN = SumNumbersMN(user_m, user_n, sumBetweenMN);
+    Console.WriteLine($"The sum of natural numbers between {user_m} and {user_n} is {sumBetweenMN}!");
+}
+else Console.WriteLine("Impossible value!");
 
 
 
